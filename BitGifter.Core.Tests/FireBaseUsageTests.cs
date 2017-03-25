60aed8ddddcf54bd6e65fd9ce7e407c8ca47d006
Artur Karbone <artur.karbone@gmail.com>
2017-03-25 07:43:36 +0200

    extract HttpClient wrapper

--- BitGifter.Core.Tests/FireBaseUsageTests.cs
+++ BitGifter.Core.Tests/FireBaseUsageTests.cs
@@ -1,7 +1,5 @@
 ﻿using CoreFire;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using Xunit;
 
 namespace BitGifter.Core.Tests
--- BitGifter.Core/BitWallet/WalletService.cs
+++ BitGifter.Core/BitWallet/WalletService.cs
@@ -1,9 +1,7 @@
 ﻿using BitGifter.Core.BitWallet.Messages;
+using BitGifter.Core.Cross_Cutting;
 using Newtonsoft.Json;
 using System;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BitGifter.Core.BitWallet
 {
@@ -53,41 +51,6 @@ namespace BitGifter.Core.BitWallet
 
             return res;
         }
-    }
+    } 
   
-    public class HttpClientWrapper
-    {
-        public HttpClient _client { get; set; } = new HttpClient();
-        public string _resource { get; set; } = string.Empty;
-        public object _param { get; set; }
-
-        public HttpClientWrapper SetBaseAddress(string baseAddress)
-        {
-            _client.BaseAddress = new Uri(baseAddress);
-            return this;
-        }
-
-        public HttpClientWrapper SetResource(string resource)
-        {
-            _resource = resource;
-            return this;
-        }
-        public HttpClientWrapper SetAuthHeader(string token)
-        {
-            _client.DefaultRequestHeaders.Add("x-api-key", token);
-            return this;
-        }
-
-        public HttpClientWrapper AddParameter<T>(T parameter) where T : class
-        {
-            _param = parameter;
-            return this;
-        }
-
-        public async Task<HttpResponseMessage> PostAsJsonAsync()
-        {
-            var jsonString = JsonConvert.SerializeObject(_param);
-            return await _client.PostAsync(_client.BaseAddress + _resource, new StringContent(jsonString, Encoding.UTF8, "application/json"));
-        }
-    }
 }
